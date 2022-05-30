88f90f098d160ff1ce0fe9de42e6e844c76767e1
Weyoss <weyoss@protonmail.com>
2022-05-30 11:35:13 +0100

    Fix missing RedisClient from package exports

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ export { LockManager } from './src/lock-manager/lock-manager';
 export { logger } from './src/logger/logger';
 export { PowerManager } from './src/power-manager/power-manager';
 export { createClientInstance } from './src/redis-client/create-client-instance';
+export { RedisClient } from './src/redis-client/redis-client';
 export { Ticker } from './src/ticker/ticker';
 export { Worker } from './src/worker/worker';
 export { WorkerRunner } from './src/worker/worker-runner/worker-runner';
